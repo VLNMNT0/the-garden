8a5eee6d368595798e50e08f5be9db854529a2dc
Valentino Montecinos <montecinosvalentino2005@hotmail.com>
2026-02-14 17:35:19 -0300

    wiped, using new lib

--- scripts/world/cutscenes/kris_room.lua
+++
@@ -1,9 +0,0 @@
-return {
-    transition_k = function (cutscene)
-        if Game:getFlag("ralsei_wakeup") == true then
-            cutscene:mapTransition("map_castletown_room_table", "room2", "up")
-        else
-            cutscene:mapTransition("map_castletown_room", "room2", "up")
-        end
-    end
-}
--- scripts/world/cutscenes/susie_room.lua
+++
@@ -1,9 +0,0 @@
-return {
-    transition_s = function (cutscene)
-        if Game:getFlag("ralsei_wakeup") == true then
-            cutscene:mapTransition("map_castletown_room_table", "room1", "up")
-        else
-            cutscene:mapTransition("map_castletown_room", "room1", "up")
-        end
-    end
-}
